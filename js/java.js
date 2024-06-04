b548679beaca8537db5eb1f1cd254454f1defc44
idahterp <idte0001@stud.kea.dk>
2024-06-04 23:34:40 +0200

    Første forsøg med menu

---
+++ js/java.js
@@ -0,0 +1,9 @@
+// Åbner når man klikker på span elementet
+function openNav (){
+    document.getElementById("myNav").style.width = "100%";
+}
+
+// lukker når man klikker på x symbolet 
+function closeNav (){
+    document.getElementById("myNav").style.width = "0%";
+}
