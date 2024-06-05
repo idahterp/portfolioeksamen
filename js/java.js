c7f440a8ca921825a70cfd15fac8747de6598287
idahterp <idte0001@stud.kea.dk>
2024-06-05 14:52:20 +0200

    opstart af website side

--- js/java.js
+++ js/java.js
@@ -1,6 +1,6 @@
 // Åbner når man klikker på span elementet
 function openNav (){
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "25%";
 }
 
 // lukker når man klikker på x symbolet 
